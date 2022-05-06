7baed6f9410ac7e7c9451fa9ee8df1cabe4e57e2
cocovs <bartcocovs@gmail.com>
2022-05-06 13:14:53 +0800

    hello

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/cocovs/learngo
+module github.com/cocovs/learngo/v1
 
 go 1.18
--- learngo.go
+++ learngo.go
@@ -5,3 +5,8 @@ import "fmt"
 func Hello() {
 	fmt.Println("hello:")
 }
+
+// Bye says bye.
+func Bye() {
+	fmt.Println("Bye go mod!")
+}
